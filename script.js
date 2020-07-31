70ac3bd3c27b0cc1365fa1f8e4de8f0e9c4d57d8
Pinois <>
2020-07-31 07:25:03 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log('HELLO');
+
+const recipes = [
+	{
+		title: 'Eggs',
+		picture: 'https://bit.ly/2ZXyiKI',
+		author: 'Loïc',
+		difficulty: 'easy',
+		timing: '15',
+		ingredients: ['eggs', 'salt', 'water'],
+		steps: [
+			'Put a pan on the fire',
+			'Crack the eggs on it',
+			'Wait, put them out',
+			'Add some salt on it',
+		],
+		id: 1596168482053,
+	},
+	{
+		title: 'Eggs',
+		picture: 'https://bit.ly/2ZXyiKI',
+		author: 'Loïc',
+		difficulty: 'easy',
+		timing: '15',
+		ingredients: ['eggs', 'salt', 'water'],
+		steps: [
+			'Put a pan on the fire',
+			'Crack the eggs on it',
+			'Wait, put them out',
+			'Add some salt on it',
+		],
+		id: 1596168522409,
+	},
+	{
+		title: 'My recipe',
+		picture: 'https://bit.ly/2ZXyiKI',
+		author: 'Loïc',
+		difficulty: 'easy',
+		timing: '15',
+		ingredients: ['eggs', 'salt', 'water'],
+		steps: [
+			'Put a pan on the fire',
+			'Crack the eggs on it',
+			'Wait, put them out',
+			'Add some salt on it',
+		],
+		id: 1596168522409,
+	},
+];
+
+const renderCard = () => {
+	// check the recipes collection
+	// generate the HTML
+	// put it in the DOM
+};
+
+const generateButton = document.querySelector('button.generate');
+generateButton.addEventListener('click', renderCard);
